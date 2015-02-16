1f7a13f546f093767d518062d645be06225bdb4d
Andreas Sandberg <andreasfrey@hotmail.com>
2015-02-16 20:54:35 +0100

    More outdata columns.

--- src/main/java/se/sandberg/ParseSpar.java
+++ src/main/java/se/sandberg/ParseSpar.java
@@ -12,6 +12,7 @@ import javax.xml.bind.Unmarshaller;
 
 import se.sparavisering.AdressTYPE;
 import se.sparavisering.AviseringsPostTYPE;
+import se.sparavisering.AvregistreringsorsakKodTYPE;
 import se.sparavisering.FolkbokforingsadressTYPE;
 import se.sparavisering.PersondetaljerTYPE;
 import se.sparavisering.SarskildPostadressTYPE;
@@ -20,44 +21,66 @@ import se.sparavisering.UtlandsadressTYPE;
 public class ParseSpar {
 
 	public static void main(String[] args) throws JAXBException, IOException {
-		if(args.length != 1){
+		if (args.length != 1) {
 			System.out.println("Usage: ParseSpar path-to-indata-file");
 			System.exit(-1);
 		}
 		File inputFile = new File(args[0]);
-		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(inputFile.getAbsolutePath()+".csv")));
-		JAXBContext jaxbContext = JAXBContext.newInstance(se.sparavisering.SPARAvisering.class);
- 
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+				inputFile.getAbsolutePath() + ".csv")));
+		JAXBContext jaxbContext = JAXBContext
+				.newInstance(se.sparavisering.SPARAvisering.class);
+		writer.write("Personnummer;Avregistreringsorsak;Sekretessmarkering;Förnamn;Tilltalsnamn (index);Efternamn;Folkbokföringsadress1;Folkbokföringsadress2;Folkbokföringspostnummer;Folkbokföringspostort;SärskildPostadress1;SärskildPostadress2;SärskildPostnummer;SärskildPostort;Utlandsadress1;Utlandsadress2;Utlandsadress3;Utlandpostkod;Utlandpostort;Land\n");
 		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-		se.sparavisering.SPARAvisering avisering = (se.sparavisering.SPARAvisering) jaxbUnmarshaller.unmarshal(inputFile);
+		se.sparavisering.SPARAvisering avisering = (se.sparavisering.SPARAvisering) jaxbUnmarshaller
+				.unmarshal(inputFile);
 		List<AviseringsPostTYPE> aviseringsPoster = avisering.getAviseringsPoster().getAviseringsPost();
-		for(AviseringsPostTYPE aviseringsPost : aviseringsPoster){
-			
+		for (AviseringsPostTYPE aviseringsPost : aviseringsPoster) {
 			writer.write(notNull(aviseringsPost.getPersonId().getFysiskPersonId()) + ";");
-
-			PersondetaljerTYPE persondetaljerTYPE = aviseringsPost.getPersondetaljer().get(0);
+			if (aviseringsPost.getPersondetaljer().get(0).getAvregistreringsorsakKod() != null) {
+				writer.write(aviseringsPost.getPersondetaljer().get(0).getAvregistreringsorsakKod() +";");
+			} else {
+				writer.write(";");
+			}
+			writer.write(notNull(aviseringsPost.getSekretessmarkering().name())
+					+ ";");
+			PersondetaljerTYPE persondetaljerTYPE = aviseringsPost
+					.getPersondetaljer().get(0);
 			writer.write(notNull(persondetaljerTYPE.getFornamn()) + ";");
-			writer.write(notNull(persondetaljerTYPE.getEfternamn())+ ";");
+			writer.write(String.valueOf(persondetaljerTYPE.getTilltalsnamn())+ ";");
+			writer.write(notNull(persondetaljerTYPE.getEfternamn()) + ";");
 
 			AdressTYPE adressTYPE = aviseringsPost.getAdress().get(0);
 			FolkbokforingsadressTYPE folkbokforingsadress = adressTYPE.getFolkbokforingsadress();
 			SarskildPostadressTYPE sarskildPostadress = adressTYPE.getSarskildPostadress();
 			UtlandsadressTYPE utlandsadress = adressTYPE.getUtlandsadress();
-			if(folkbokforingsadress != null && (folkbokforingsadress.getUtdelningsadress1() != null || folkbokforingsadress.getUtdelningsadress2() != null)){
-				writer.write(notNull(folkbokforingsadress.getUtdelningsadress1())+ " ");
-				writer.write(notNull(folkbokforingsadress.getUtdelningsadress2())+ ";");
-				writer.write(notNull(folkbokforingsadress.getPostNr())+ ";");
-				writer.write(notNull(folkbokforingsadress.getPostort())+ ";");
-			}else if(sarskildPostadress != null && (sarskildPostadress.getUtdelningsadress1() != null || sarskildPostadress.getUtdelningsadress2() != null)){
-				writer.write(notNull(sarskildPostadress.getUtdelningsadress1())+ " ");
+			if (folkbokforingsadress != null) {
+				writer.write(notNull(folkbokforingsadress.getUtdelningsadress1()) + ";");
+				writer.write(notNull(folkbokforingsadress.getUtdelningsadress2()) + ";");
+				writer.write(notNull(folkbokforingsadress.getPostNr()) + ";");
+				writer.write(notNull(folkbokforingsadress.getPostort()) + ";");
+			} else {
+				writer.write(";;;;");
+			}
+
+			if (sarskildPostadress != null) {
+				writer.write(notNull(sarskildPostadress.getUtdelningsadress1())+ ";");
 				writer.write(notNull(sarskildPostadress.getUtdelningsadress2())+ ";");
-				writer.write(notNull(sarskildPostadress.getPostNr())+ ";");
-				writer.write(notNull(sarskildPostadress.getPostort())+ ";");
-			}else if(utlandsadress != null && (utlandsadress.getUtdelningsadress1() != null || utlandsadress.getUtdelningsadress2() != null)){
-				writer.write(notNull(utlandsadress.getUtdelningsadress1())+ " ");
+				writer.write(notNull(sarskildPostadress.getPostNr()) + ";");
+				writer.write(notNull(sarskildPostadress.getPostort()) + ";");
+			} else {
+				writer.write(";;;;");
+			}
+
+			if (utlandsadress != null) {
+				writer.write(notNull(utlandsadress.getUtdelningsadress1())+ ";");
 				writer.write(notNull(utlandsadress.getUtdelningsadress2())+ ";");
-				writer.write(notNull(utlandsadress.getPostKodUtland())+ ";");
-				writer.write(notNull(utlandsadress.getPostortUtland())+ ";");
+				writer.write(notNull(utlandsadress.getUtdelningsadress3())+ ";");
+				writer.write(notNull(utlandsadress.getPostKodUtland()) + ";");
+				writer.write(notNull(utlandsadress.getPostortUtland()) + ";");
+				writer.write(notNull(utlandsadress.getLand()) + ";");
+			} else {
+				writer.write(";;;;");
 			}
 			writer.write("\n");
 		}
